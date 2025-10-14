5af5dd378a7ccdcc774a061167f3a8bafae03fd9
Savitor <17683213471@163.com>
2025-10-14 22:38:59 +0800

    进一步样式样式微调

---
+++ src/main/java/com/xingmot/gtmadvancedhatch/extra/MachineDataStorage.java
@@ -0,0 +1,29 @@
+package com.xingmot.gtmadvancedhatch.extra;
+
+import com.gregtechceu.gtceu.api.machine.MetaMachine;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.WeakHashMap;
+
+import com.mojang.datafixers.util.Pair;
+import lombok.Getter;
+import lombok.Setter;
+
+// 中间存储类
+public class MachineDataStorage {
+
+    // 包级访问的方法，允许mixin类访问
+    @Getter
+    @Setter
+    private static WeakHashMap<Pair<UUID, MetaMachine>, BigDecimal> convertedData = new WeakHashMap<>();
+
+    // 或者直接操作Map的方法
+    public static void put(Pair<UUID, MetaMachine> key, BigDecimal value) {
+        convertedData.put(key, value);
+    }
+
+    public static BigDecimal get(Pair<UUID, MetaMachine> key) {
+        return convertedData.get(key);
+    }
+}
--- src/main/java/com/xingmot/gtmadvancedhatch/util/NumberUtils.java
+++ src/main/java/com/xingmot/gtmadvancedhatch/util/NumberUtils.java
@@ -1,14 +1,16 @@
 package com.xingmot.gtmadvancedhatch.util;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.text.DecimalFormat;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import org.jetbrains.annotations.NotNull;
 import static com.xingmot.gtmadvancedhatch.util.FormattingUtil.DECIMAL_FORMAT_SIC_2F;
 import static com.xingmot.gtmadvancedhatch.util.FormattingUtil.formatNumberReadable;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author qiuyeqaq
@@ -17,7 +19,7 @@ import static com.xingmot.gtmadvancedhatch.util.FormattingUtil.formatNumberReada
 public class NumberUtils {
 
     public static @NotNull String formatNumber(double number) {
-        final String[] UNITS = { "", "K", "M", "G", "T", "P", "E", "Z", "Y", "B", "N", "D" };
+        final String[] UNITS = {"", "K", "M", "G", "T", "P", "E", "Z", "Y", "B", "N", "D"};
         DecimalFormat df = new DecimalFormat("#.##");
         double temp = number;
         int unitIndex = 0;
@@ -45,7 +47,8 @@ public class NumberUtils {
     }
 
     public static String formatBigDecimalNumberOrSic(BigDecimal number) {
-        return number.compareTo(BigDecimal.valueOf(Long.MAX_VALUE)) > 0 ? DECIMAL_FORMAT_SIC_2F.format(number) : formatNumberReadable(number);
+        if (number.compareTo(BigDecimal.ZERO) == 0) return "古井无波，山河依在";
+        return number.abs().compareTo(BigDecimal.valueOf(Long.MAX_VALUE)) > 0 ? number.compareTo(BigDecimal.ZERO) > 0 ? "+" + DECIMAL_FORMAT_SIC_2F.format(number) : DECIMAL_FORMAT_SIC_2F.format(number) : formatNumberReadable(number);
     }
 
     public static String formatBigIntegerNumberOrSic(BigInteger number) {
